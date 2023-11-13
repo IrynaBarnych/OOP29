4d200cd914e6563be37b58658f537dbbdf128d38
IrynaBarnych <iryna.barnych@gmail.com>
2023-11-13 20:48:14 +0200

    analog_clock digital_clock

--- main.py
+++ main.py
@@ -1,59 +1,18 @@
-#успадкування
-class Animal:
-    def sound(self):
-        print("гучно оре")
-class Dog(Animal):
-    def sound(self):
-        super().sound()
-        print("гавкає")
-dog = Dog()
-dog.sound()
+class Clock:
+    def __init__(self, hours, minutes):
+        self.hours = hours
+        self.minutes = minutes
+    def display_times(self):
+        print(f'{self.hours:02d}:{self.minutes:02d}')
+class AnalogClock(Clock):
+    def display_times(self):
+        super().display_times()
 
-class Parent:
-    def __init__(self, name):
-        self.name = name
-        print("Корисна річ")
-class Clild(Parent):
-    def __init__(self, name, additional_info):
-        #self.name = name
-        super().__init__(name) #виклик конструктора батьківського класу
-        self.additional_info = additional_info
-child = Clild("Oleg", "information")
-print(child.name, child.additional_info)
+class DigitalClock(Clock):
+    def display_times(self):
+        super().display_times()
 
-#система школа
-class Person:
-    def __init__(self, name, age, gender):
-        self.name = name
-        self.age = age
-        self.gender = gender
-class Student(Person):
-    def __init__(self, name, age, gender, student_id):
-        super().__init__(name, age, gender)
-        self.student_id = student_id
-    def get_student_id(self):
-        return self.student_id
-    def display_subject(self, subjects):
-        print(f"студент вивчив {self.name} предмети: ")
-        for subject in subjects:
-            print(subject)
-class Teacher(Person):
-    def __init__(self, name, age, gender, employee_id):
-        super().__init__(name, age, gender)
-        self.employee_id = employee_id
-    def get_employee_id(self):
-        return self.__get_employee_id
-
-    def add_grades(self, student, grades):
-        print(f'Вчитель {self.name} виставив оцінку для {student.name}')
-        for subject, grade in grades.items():
-            print(f"по предмету {subject} оцінка {grade}")
-
-
-student1 = Student("Oleg", 15, "male", 12)
-student2 = Student("Olga", 16, "female", 13)
-print(f"Студент на ім'я {student1.name} з id: {student1.get_student_id}")
-student1.display_subject(['Math', 'Scien', 'History'])
-
-teacher1 = Teacher('пан Михайло', 35, "male", 125)
-teacher1.add_grades(student1, {"Math": "A", "Science": "B", "Histore": "C"})
+analog_clock = AnalogClock(1, 45)
+digital_clock = DigitalClock(15, 43)
+analog_clock.display_times()
+digital_clock.display_times()
