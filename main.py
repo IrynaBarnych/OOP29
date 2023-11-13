50df2945ed1721d76f790492585f170ea9da77b1
IrynaBarnych <iryna.barnych@gmail.com>
2023-11-13 19:45:22 +0200

    Student(Person) + Teacher(Person)

--- main.py
+++ main.py
@@ -20,3 +20,20 @@ class Clild(Parent):
         self.additional_info = additional_info
 child = Clild("Oleg", "information")
 print(child.name, child.additional_info)
+
+#система школа
+class Person:
+    def __init__(self, name, age, gender):
+        self.name = name
+        self.age = age
+        self.gender = gender
+class Student(Person):
+    def __init__(self, name, age, gender, student_id):
+        super().__init__(self, name, age, gender)
+        self.student_id = student_id
+class Teacher(Person):
+    def __init__(self, name, age, gender, employee_id):
+        super().__init__(self, name, age, gender)
+        self.employee_id = employee_id
+
+
