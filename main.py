99383b16abbc86568b0bbf0fb80545439f83a9c6
IrynaBarnych <iryna.barnych@gmail.com>
2023-11-13 21:18:11 +0200

    виправила код

--- main.py
+++ main.py
@@ -1,35 +1,35 @@
 class Animal:
-    def __init__(self, name,species, sound):
+    def __init__(self, name, species, sound):
         self.name = name
         self.sound = sound
         self.species = species
 
     def make_sound(self):
-        print(f'{self.name} каже {self.sound}')
+        print(f'{self.species} {self.name} каже {self.sound}')
     def move(self):
         print(f'{self.species} {self.name} рухатися')
-    def move(self):
-        print(f'{self.species} {self.name} плаває')
+    def swim(self):
+        print(f"{self.species} {self.name} плаває")
 
 class Tiger(Animal):
     def __init__(self, name):
-        super().__init__(name, "Tiger", "Road")
+        super().__init__(name, "Tiger", "Roar")
 
     def swim(self):
         super().swim()
 
-
-class Crocodile(Animal):
+class Crocodie(Animal):
     def __init__(self, name):
-        super().__init__(name, "Crocodale", "Rrrr")
+        super().__init__(name, "Crocodie", "Rrrrrrrr")
 
     def swim(self):
         super().swim()
 
-tiger =Tiger ("Loki")
-crocodie = Crocodile("Sherhan")
+tiger = Tiger("Шерхан")
+crocodie = Crocodie("Локі")
 crocodie.swim()
 tiger.swim()
+crocodie.make_sound()
 
 
 
