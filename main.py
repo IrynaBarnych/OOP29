b75ebd22340c35c9d3498c3f1e979d69d8a48cd0
IrynaBarnych <iryna.barnych@gmail.com>
2023-11-13 19:26:11 +0200

    виклик конструктора батьківського класу

--- main.py
+++ main.py
@@ -8,3 +8,15 @@ class Dog(Animal):
         print("гавкає")
 dog = Dog()
 dog.sound()
+
+class Parent:
+    def __init__(self, name):
+        self.name = name
+        print("Корисна річ")
+class Clild(Parent):
+    def __init__(self, name, additional_info):
+        #self.name = name
+        super().__init__(name) #виклик конструктора батьківського класу
+        self.additional_info = additional_info
+child = Clild("Oleg", "information")
+print(child.name, child.additional_info)
