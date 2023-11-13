5fe10faf10500cc0ffd5fbf127d8219a7d6c07e4
IrynaBarnych <iryna.barnych@gmail.com>
2023-11-13 19:16:14 +0200

    + super

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ class Animal:
     def sound(self):
         print("гучно оре")
 class Dog(Animal):
-    pass
+    def sound(self):
+        super().sound()
+        print("гавкає")
 dog = Dog()
 dog.sound()
