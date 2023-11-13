fefb3ca3d077e518a8615a0cd012854683cdd0e6
IrynaBarnych <iryna.barnych@gmail.com>
2023-11-13 20:16:35 +0200

    Student(Person) + Teacher(Person)

--- main.py
+++ main.py
@@ -29,11 +29,19 @@ class Person:
         self.gender = gender
 class Student(Person):
     def __init__(self, name, age, gender, student_id):
-        super().__init__(self, name, age, gender)
+        super().__init__(name, age, gender)
         self.student_id = student_id
+    def get_student_id(self):
+        return self.student_id
+    def display_subject(self, subjects):
+        print(f"студент вивчив {self.name} предмети: ")
+        for subject in subjects:
+            print(subject)
 class Teacher(Person):
     def __init__(self, name, age, gender, employee_id):
         super().__init__(self, name, age, gender)
         self.employee_id = employee_id
-
-
+student1 = Student("Oleg", 15, "male", 12)
+student2 = Student("Olga", 16, "female", 13)
+print(f"Студент на ім'я {student1.name} з id: {student1.get_student_id}")
+student1.display_subject(['Math', 'Scien', 'History'])
