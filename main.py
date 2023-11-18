ca24f50b40e940db98682636ccc916ebcae1f214
IrynaBarnych <iryna.barnych@gmail.com>
2023-11-18 18:33:37 +0200

    додала інформацію про Людину і будівельника

--- main.py
+++ main.py
@@ -1,17 +1,16 @@
-# Завдання 2
-# Створіть клас Passport (паспорт), який міститиме паспортну інформацію про громадянина заданої країни.
-# За допомогою механізму успадкування реалізуйте клас ForeignPassport (закордонний паспорт), похідний
-# від Passport.
-# Нагадаємо, що закордонний паспорт містить, крім паспортних даних, дані про візи і номер закордонного
-# паспорта. Кожен із класів має містити необхідні методи.
+# Завдання 1
+# Створіть клас Human, який міститиме інформацію про людину.
+# За допомогою механізму успадкування реалізуйте клас Builder (містить інформацію про будівельника),
+# клас Sailor (містить інформацію про моряка), клас Pilot (містить інформацію про льотчика).
+# Кожен із класів має містити необхідні для роботи методи.
 
 
-class Passport:
-    def __init__(self, name, date_of_birth, citizenship, registration):
+class Human:
+    def __init__(self, name, date_of_birth, registration, citizenship):
         self.name = name
         self.date_of_birth = date_of_birth
-        self.citizenship = citizenship
         self.registration = registration
+        self.citizenship = citizenship
 
     def display_info(self):
         print(f'Ім\'я: {self.name}')
@@ -32,37 +31,27 @@ class Passport:
         print(f"{self.name} селі Моринці, Житомирська область")
 
 
-class ForeignPassport(Passport):
-    def __init__(self, name, date_of_birth, citizenship, registration, data_on_visas, number_foreign_passport):
-        super().__init__(name, date_of_birth, citizenship, registration)
-        self.data_on_visas = data_on_visas
-        self.number_foreign_passport = number_foreign_passport
+class Builder:
+    def __init__(self, name, certificate, experience, qualification):
+        self.name = name
+        self.certificate = certificate
+        self.experience = experience
+        self.qualification = qualification
 
     def display_info(self):
         super().display_info()
-        print(f'Інформація про візи: {self.data_on_visas}')
-        print(f'Номер закордонного паспорта: {self.number_foreign_passport}')
+        print(f'Інформація про сертифікат: {self.certificate}')
+        print(f'Інформація про досвід роботи: {self.experience}')
+        print(f'Інформація про кваліфікацію: {self.qualification}')
 
-    def new_data_on_visas(self):
+    def new_experience(self):
         print(f"{self.name} ")
 
-    def new_number_foreign_passport(self):
+    def new_qualification(self):
         print(f"{self.name} ")
 
 
-passport_info = Passport("Тарас Григорович Шевченко", "09.03.1814",
-                         "Україна", "селі Моринці, Житомирська область")
 
-foreign_passport_info = ForeignPassport("Тарас Григорович Шевченко", "09.03.1814",
-                                       "Україна", "селі Моринці, Житомирська область",
-                                       "Дані про візи",
-                                       "Номер закордонного паспорта")
-
-print("Інформація звичайного паспорта:")
-passport_info.display_info()
-
-print("\nІнформація закордонного паспорта:")
-foreign_passport_info.display_info()
 
 
 
